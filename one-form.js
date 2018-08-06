7c7dad62988571f0c35769cf9b9410d88c8f7ef8
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2018-08-06 16:15:39 +0100

    Removed drupal behaviors
    Removed the drupal behaviors command as this would require us to access the module which is not possible for us to do on Aegir so this command while in-depth would not be useful

--- one-form.js
+++ one-form.js
@@ -1,22 +1,3 @@
-Drupal.behaviors.formSingleSubmit = {
-  attach: function (){
-    function onFormSubmit (e) {
-      var $form = $(e.currentTarget);
-      var formValues = $form.serialize();
-      var previousValues = $form.attr('data-drupal-form-submit-last');
-      if (previousValues === formValues){
-        e.preventDefault();
-      }
-      else{
-        $form.attr('data-drupal-form-submit-last', formValues);
-      }
-    }
-
-    $('body').once('form-single-submit')
-      .on('submit.singleSubmit', 'form:not([method~="GET"])', onFormSubmit);
-  }
-};
-
 $(function () {
     var clickCount = 0;
     $("#edit-submit").click(function(event){
