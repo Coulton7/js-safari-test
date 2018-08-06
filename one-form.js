a79f83b4b776ae53ebf207e2edd9829d6c920abb
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2018-08-06 14:36:21 +0100

    Another js code based on a patch but may need to be looked at further

--- one-form.js
+++ one-form.js
@@ -10,3 +10,23 @@ jQuery.fn.preventDoubleSubmission = function(){
     });
     return this;
 };
+
+
+Drupal.behaviors.formSingleSubmit = {
+  attach: function (){
+    function onFormSubmit (e) {
+      var $form = $(e.currentTarget);
+      var formValues = $form.serialize();
+      var previousValues = $form.attr('data-drupal-form-submit-last');
+      if (previousValues === formValues){
+        e.preventDefault();
+      }
+      else{
+        $form.attr('data-drupal-form-submit-last', formValues);
+      }
+    }
+
+    $('body').once('form-single-submit')
+      .on('submit.singleSubmit', 'form:not([method~="GET"])', onFormSubmit);
+  }
+};
