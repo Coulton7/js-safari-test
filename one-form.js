cd79517e75b2f00ce8d38bc87d7712625b921cc1
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2018-08-06 16:22:33 +0100

    Added first function to the start of the script
    Needed an intital unction at the start in order for the js to work as it would $ as undefined variable thus the script would not work

--- one-form.js
+++ one-form.js
@@ -1,11 +1,15 @@
-$(function () {
+(function($){
+
+  $(function () {
     var clickCount = 0;
     $("#edit-submit").click(function(event){
         if(clickCount++ > 0) {
             event.preventDefault()
         }
       });
+    });
 });
+
 
 function aesinternational_form_alter(&$form, &$form_state, &$form_id) {
 drupal_add_js(drupal_get_path('aesinternational','js').'one-form.js')
