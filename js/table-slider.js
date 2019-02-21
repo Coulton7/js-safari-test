1bfc27f5ef39a5a82293b77254b17de0cd87400c
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-02-21 11:31:32 +0000

    Adding the imperial script on to the form for the testing

--- js/table-slider.js
+++ js/table-slider.js
@@ -40,5 +40,46 @@ $(document).ready(function() {
     $("#amount").val($("#slider-range").slider("values", 0) +
       " - " + $("#slider-range").slider("values", 1) + "mm");
   });
+  $(function() {
+
+    var impcolArray = $('#imperial-table .table td:nth-child(2)').map(function() {
+      return $(this).text();
+    }).get();
+
+    var implastVal = impcolArray.length - 1;
+    var impminVal = parseFloat(impcolArray[0]);
+    var impmaxVal = parseFloat(impcolArray[implastVal]);
+    $("#slider-range-imp").slider({
+      range: true,
+      min: impminVal,
+      max: impmaxVal,
+      step: 0.01,
+      values: [impminVal, impmaxVal],
+      slide: function(event, ui) {
+        var table = document.getElementById("imperial-table").getElementsByClassName("tablefield")[0];
+        // in this function we can define what happens when a user changes the sliders
+        $("#amount-imp").val(ui.values[0] + " - " + ui.values[1] + " Inches");
+
+        for (var i = 1, row; row = table.rows[i]; i++) {
+          //iterate through rows (we SKIP the first row: counter starts at 1!)
+          for (var j = 0, col; col = row.cells[j]; j++) {
+            //iterate through columns: if first column not in range: HIDE, else SHOW
+
+            if (j == 1) { // if first column
+              if ($(col).html() >= ui.values[0] && $(col).html() <= ui.values[1]) {
+                // if in interval
+                $(row).show();
+              } else {
+                $(row).hide();
+              }
+            }
+          }
+        }
+      }
+    });
+
+    $("#amount-imp").val($("#slider-range-imp").slider("values", 0) +
+      " - " + $("#slider-range-imp").slider("values", 1) + "inches");
+  });
 
 });
