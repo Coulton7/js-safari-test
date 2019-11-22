1706225591e841b4607e1ae51bd1219f94be94a7
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-11-22 15:28:12 +0000

    test again

--- js/reg-mark.js
+++ js/reg-mark.js
@@ -19,8 +19,7 @@
           return;
         }
       }
-      html = element.data.replace(/\bWarman\b/gi, "$&<sup>&reg;</sup>");
-      html = element.data.replace('#&reg;(?!\s*</sup>|[^<]*>)#','<sup>&reg;</sup>');
+      html = element.data.replace(/\bWarman\b|\b&reg;\b/gi, "$&<sup>&reg;</sup>");
       var frag = (function() {
         var wrap = document.createElement('div'),
           frag = document.createDocumentFragment();
