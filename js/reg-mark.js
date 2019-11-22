b077d42d3d95e8ff874b7cc7eeec390b9b7ad4d2
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-11-22 15:55:03 +0000

    adjusted to try something

--- js/reg-mark.js
+++ js/reg-mark.js
@@ -1,10 +1,11 @@
 (function($) {
   $(document).ready(function() {
-    $('body :not(script)').contents().filter(function() {
+    $('warman').contents().filter(function() {
     return this.nodeType === 3;
     }).replaceWith(function() {
-    return this.nodeValue.replace(/[™®©]/g, ' ');
+    return this.nodeValue.replace(/[™®©]/g, '');
     });
+
     function recurse(element) {
       if (element.childNodes.length > 0) {
         for (var i = 0; i < element.childNodes.length; i++) {
