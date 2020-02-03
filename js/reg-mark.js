40264677903fddc884250bbc172edfbc872f905f
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2020-02-03 15:31:49 +0000

    Adding another function in to see if it will work

--- js/reg-mark.js
+++ js/reg-mark.js
@@ -1,5 +1,12 @@
 (function($) {
   $(document).ready(function() {
+
+    $("body").html(
+      $("body").html().replace(/&reg;/gi, '<sup>&reg;</sup>').replace(/®/gi, '<sup>&reg;</sup>')); // this will break in script tags and a few other places.
+      $("sup sup").each(function () {  // find sup with child sup
+      $(this).parent("sup").html("&reg;");
+    });
+
     function recurse(element) {
       if (element.childNodes.length > 0) {
         for (var i = 0; i < element.childNodes.length; i++) {
