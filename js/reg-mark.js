8c83a0eef7a47c03c051f5ebfb24c220e4bc35ec
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2020-02-03 15:23:14 +0000

    Changing the order for testing purposes

--- js/reg-mark.js
+++ js/reg-mark.js
@@ -19,8 +19,9 @@
           return;
         }
       }
-      html = element.data.replace(/\bWarman\b/gi, "$&<sup>&reg;</sup>")
-      .replace(/((?!<sup>\s*))®((?!\s*<\/sup>))/gi, '<sup>&reg;</sup>');
+      html = element.data
+      .replace(/((?!<sup>\s*))®((?!\s*<\/sup>))/gi, '<sup>&reg;</sup>')
+      .replace(/\bWarman\b/gi, "$&<sup>&reg;</sup>");
       var frag = (function() {
         var wrap = document.createElement('div'),
           frag = document.createDocumentFragment();
