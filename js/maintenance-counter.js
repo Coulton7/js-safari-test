986a9c2973ccf8fb12f8bde7f767bc42a8c06714
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2020-02-03 11:45:43 +0000

    not worked resetting

--- js/maintenance-counter.js
+++ js/maintenance-counter.js
@@ -2,8 +2,6 @@ function countdown(){
   var i = document.getElementById('counter');
   if (parseInt(i.innerHTML)<=0){
 
-  document.body.innerHTML = document.body.innerHTML.replace("to the gloabl website", "If re-direct has failed click the link to the site");
-
   }
   if (parseInt(i.innerHTML)!=0){
     i.innerHTML = parseInt(i.innerHTML)-1;
