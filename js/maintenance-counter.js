5fcbe16c60d96e69ffdda325f09b23862a6a1dd3
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2020-02-03 11:41:18 +0000

    testing new method to replace the text

--- js/maintenance-counter.js
+++ js/maintenance-counter.js
@@ -2,6 +2,8 @@ function countdown(){
   var i = document.getElementById('counter');
   if (parseInt(i.innerHTML)<=0){
 
+  document.body.innerHTML = document.body.innerHTML.replace("to the gloabl website", "If re-direct has failed click the link to the site");
+
   }
   if (parseInt(i.innerHTML)!=0){
     i.innerHTML = parseInt(i.innerHTML)-1;
