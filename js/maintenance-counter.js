0c8e18e8c4edd5d105fe07e26358214b33f75832
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2020-02-03 11:37:51 +0000

    Trying the new text method see if it works need to check warning

--- js/maintenance-counter.js
+++ js/maintenance-counter.js
@@ -1,7 +1,9 @@
 function countdown(){
   var i = document.getElementById('counter');
   if (parseInt(i.innerHTML)<=0){
-    
+    var text = "to the global website";
+    var new_text = text.replace("to the gloabl website", "If re-direct has failed click thelink to the site");
+    document.write(new_text);
   }
   if (parseInt(i.innerHTML)!=0){
     i.innerHTML = parseInt(i.innerHTML)-1;
