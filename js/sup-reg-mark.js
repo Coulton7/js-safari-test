ba7c5868f11521ce138a8163438f8b0e6796a8ba
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2020-02-04 12:39:33 +0000

    putting it under one document.ready

--- js/sup-reg-mark.js
+++ js/sup-reg-mark.js
@@ -5,11 +5,6 @@
     }).replaceWith(function() {
         return this.nodeValue.replace(/[™®©]/gi, '<sup>$&</sup>');
     });
-  });
-})(jQuery);
-
-(function($) {
-  $(document).ready(function() {
-    $(document.body).html($(document.body).html().replace(/((<sup>\s*))®((\s*<\/sup>))\s((<sup>\s*))®((\s*<\/sup>))/gi, "<sup>&reg;</sup>"));
+        $(document.body).html($(document.body).html().replace(/((<sup>\s*))®((\s*<\/sup>))\s((<sup>\s*))®((\s*<\/sup>))/gi, "<sup>&reg;</sup>"));
   });
 })(jQuery);
