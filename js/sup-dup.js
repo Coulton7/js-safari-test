2f51922ad958f458c3905b864408be0a0e5e1774
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2020-02-04 12:44:59 +0000

    Script has not workedd removing the script completely

--- js/sup-dup.js
+++
@@ -1,5 +0,0 @@
-(function($) {
-  $(document).ready(function() {
-    $(document.body).html($(document.body).html().replace(/((<sup>\s*))®((\s*<\/sup>))\s((<sup>\s*))®((\s*<\/sup>))/gi, "<sup>&reg;</sup>"));
-  });
-})(jQuery);
