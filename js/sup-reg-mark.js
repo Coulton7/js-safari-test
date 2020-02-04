00e8b90c187313d68122c5f406fbad99bbafcefc
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2020-02-04 11:39:55 +0000

    Editied script for the tag

--- js/sup-reg-mark.js
+++ js/sup-reg-mark.js
@@ -3,7 +3,11 @@
     $('body :not(script)').contents().filter(function() {
         return this.nodeType === 3;
     }).replaceWith(function() {
-        if( this.nodeValue.replace(/\bWarman\b/gi, "$&<sup>&reg;</sup>"));
+      if(this.nodeValue == /Warman<sup>&reg;<\/sup>/gi ){
+        return;
+      }else{
+        this.nodeValue.replace(/\bWarman\b/gi, "$&<sup>&reg;</sup>");
+      }
         return this.nodeValue.replace(/[™®©]/gi, '<sup>$&</sup>');
     });
   });
