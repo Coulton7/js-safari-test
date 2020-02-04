a8e24d274c035290f3e700e08a55f645e25768fc
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2020-02-04 11:45:49 +0000

    reset the javascript need to find correct if statement

--- js/sup-reg-mark.js
+++ js/sup-reg-mark.js
@@ -3,11 +3,6 @@
     $('body :not(script)').contents().filter(function() {
         return this.nodeType === 3;
     }).replaceWith(function() {
-      if(this.nodeValue == /\bWarman<sup>&reg;<\/sup>\b/gi ){
-        return;
-      }else{
-        this.nodeValue.replace(/\bWarman\b/gi, "$&<sup>&reg;</sup>");
-      }
         return this.nodeValue.replace(/[™®©]/gi, '<sup>$&</sup>');
     });
   });
