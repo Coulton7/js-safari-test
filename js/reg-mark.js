03c1a156609268d335be5ac9a3a60eed4422b86d
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2020-02-04 15:16:47 +0000

    Testing a new theory

--- js/reg-mark.js
+++ js/reg-mark.js
@@ -19,8 +19,8 @@
           return;
         }
       }
-      html = element.data.replace(/\bWarman\b/gi, "$&<sup>&reg;</sup>");
-      html = html.replace(/\bWarman[®]\b/gi, 'WARMAN');
+      html = element.data.replace(/\bWarman\b/gi, "&reg;");
+      html = html.replace(/[®]/gi, '<sup>$&</sup>');
       var frag = (function() {
         var wrap = document.createElement('div'),
           frag = document.createDocumentFragment();
