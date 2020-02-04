754d4ee8a35ae2f3db442421563df1fa0ec6f7aa
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2020-02-04 11:49:30 +0000

    trying a new test variation which shoudl counter the double reg-mark

--- js/sup-reg-mark.js
+++ js/sup-reg-mark.js
@@ -7,3 +7,13 @@
     });
   });
 })(jQuery);
+
+(function($) {
+  $(document).ready(function() {
+    $('body :not(script)').contents().filter(function() {
+        return this.nodeType === 3;
+    }).replaceWith(function() {
+        return this.nodeValue.replace(/[®®]/gi, '<sup>®</sup>');
+    });
+  });
+})(jQuery);
