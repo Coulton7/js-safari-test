41bb0bc996560cdd9567367c2672dad82485e8ca
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2020-02-04 15:10:22 +0000

    Testing

--- js/reg-mark.js
+++ js/reg-mark.js
@@ -20,7 +20,7 @@
         }
       }
       html = element.data.replace(/\bWarman\b/gi, "$&<sup>&reg;</sup>");
-      html = html.replace(/[®©™]/gi, '');
+      html = html.replace(/\bWarman®\b/gi, 'Warman');
       var frag = (function() {
         var wrap = document.createElement('div'),
           frag = document.createDocumentFragment();
