6567af2b02cb554754036a375d6ccdd110656ee9
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2020-02-18 11:43:57 +0000

    Testing new timer variation code to see its effect

--- js/vex-timer.js
+++ js/vex-timer.js
@@ -1,34 +1,41 @@
 (function($) {
-  vex.defaultOptions.className = 'vex-theme-top';
-
   $(document).ready(function() {
+    function countdown(seconds) {
+      seconds = parseInt(sessionStorage.getItem("seconds")) || seconds;
 
-    setTimeout(function(){
-
-    if (sessionStorage.getItem('vexClosed') === 'closed') {
-      return null;
-    } else {
-        vex.dialog.open({
-          message: "Can't find what you are looking for maybe try going to aesseal.com",
-          contentClassName: 'web-banner text-center',
-          buttons: [
-            $.extend({}, vex.dialog.buttons.YES, {
-              className: 'col-lg-4 col-lg-pull-2 col-xs-5 col-xs-pull-1 btn-red vex-ext',
-              text: 'Go to aesseal.com',
-              click: function() {
-                window.open('https://www.aesseal.com/', '_blank');
+      function tick() {
+        seconds--;
+        sessionStorage.setItem("seconds", seconds);
+        if (seconds > 0) {
+          setTimeout(tick, 1000);
+        }
+        else{
+          if (sessionStorage.getItem('vexClosed') != 'closed') {
+            vex.dialog.open({
+              message: "Can't find what you are looking for maybe try going to aesseal.com",
+              contentClassName: 'text-center',
+              buttons: [
+                $.extend({}, vex.dialog.buttons.YES, {
+                  className: 'col-lg-4 col-lg-pull-2 col-xs-5 col-xs-pull-1 btn-red',
+                  text: 'Go to aesseal.com',
+                  click: function() {
+                    window.location.href = 'https://www.aesseal.com/';
+                  }
+                }),
+                $.extend({}, vex.dialog.buttons.NO, {
+                  className: 'col-xs-5 col-xs-pull-1 col-lg-4 col-lg-pull-2 btn-default',
+                  text: 'Remain on this site',
+                })
+              ],
+              afterClose: function() {
+                sessionStorage.setItem('vexClosed', 'closed');
               }
-            }),
-            $.extend({}, vex.dialog.buttons.NO, {
-              className: 'col-xs-5 col-xs-pull-1 col-lg-4 col-lg-pull-2 btn-midgrey vex-closes',
-              text: 'Remain on this site',
-            })
-          ],
-          afterClose: function() {
-            sessionStorage.setItem('vexClosed', 'closed');
+            });
           }
-        });
+        }
+        tick();
       }
-    }, 10000);
+    }
+    countdown(10);
   });
 })(jQuery);
