0f1cd08392e9ae6aaebd5a011f2038466dd59beb
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2020-02-18 11:54:56 +0000

    Adding a new if startement mostlikely won't work

--- js/vex-timer.js
+++ js/vex-timer.js
@@ -37,5 +37,29 @@
       }
     }
     vex_counter(10);
+    if(vex_counter(seconds == 0)){
+      if (sessionStorage.getItem('vexClosed') != 'closed') {
+        vex.dialog.open({
+          message: "Can't find what you are looking for maybe try going to aesseal.com",
+          contentClassName: 'text-center',
+          buttons: [
+            $.extend({}, vex.dialog.buttons.YES, {
+              className: 'col-lg-4 col-lg-pull-2 col-xs-5 col-xs-pull-1 btn-red',
+              text: 'Go to aesseal.com',
+              click: function() {
+                window.location.href = 'https://www.aesseal.com/';
+              }
+            }),
+            $.extend({}, vex.dialog.buttons.NO, {
+              className: 'col-xs-5 col-xs-pull-1 col-lg-4 col-lg-pull-2 btn-default',
+              text: 'Remain on this site',
+            })
+          ],
+          afterClose: function() {
+            sessionStorage.setItem('vexClosed', 'closed');
+          }
+        });
+      }
+    }
   });
 })(jQuery);
