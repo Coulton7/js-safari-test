c5f7f50290604ec4ac348cb4dbbcefdaed8148b6
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2020-03-03 12:21:43 +0000

    editing the script sligltly to see what effect it has

--- js/reg-mark.js
+++ js/reg-mark.js
@@ -20,7 +20,7 @@
         }
       }
       html = element.data.replace(/\bWarman\b/gi, "$&<sup>&reg;</sup>");
-      html = html.replace(/\b®\b/gi, "");
+      html = html.replace(/®/gi, "");
       var frag = (function() {
         var wrap = document.createElement('div'),
           frag = document.createDocumentFragment();
