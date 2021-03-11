59de92b6cc9f46c19e711fc49b01c577935801b6
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-03-11 15:14:34 +0000

    Missed off quotation marks for the svg

--- js/bs-init.js
+++ js/bs-init.js
@@ -3,7 +3,7 @@
   });
 
   $(document).ready(function() {
-    var allRegions = $(svg.uk > * );
+    var allRegions = $("svg.uk > *");
 
     allRegions.on("click", function() {
       allRegions.removeClass("on");
