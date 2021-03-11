48645f72706ca3d2fefa86336a4cfd53d86e5f37
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-03-11 15:36:28 +0000

    Trsting a different method slecting the regions

--- js/bs-init.js
+++ js/bs-init.js
@@ -3,7 +3,7 @@
   });
 
   $(document).ready(function() {
-    var allRegions = $("svg.uk switch g g#map");
+    var allRegions = $("g#map > *");
 
     allRegions.on("click", function() {
       allRegions.removeClass("on");
