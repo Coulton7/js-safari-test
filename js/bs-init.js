98b5e9e6cdf0fc6805ddedf86ffcc7250abed37b
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-03-11 15:21:17 +0000

    Redefining in the script on what o target

--- js/bs-init.js
+++ js/bs-init.js
@@ -3,7 +3,7 @@
   });
 
   $(document).ready(function() {
-    var allRegions = $("svg.uk > *");
+    var allRegions = $("svg.uk > #map");
 
     allRegions.on("click", function() {
       allRegions.removeClass("on");
