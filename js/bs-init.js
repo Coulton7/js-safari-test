8f94d902f673099014833fab6b89afc97fd29536
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-03-16 14:34:12 +0000

    Checking to see if the js is movable

--- js/bs-init.js
+++ js/bs-init.js
@@ -8,6 +8,13 @@
     allRegions.on("click", function() {
       allRegions.removeClass("on");
       $(this).addClass("on");
+      $('.location-pop-up').dialog({
+        width: 600,
+        height: 'auto',
+        modal:true,
+        title: 'Location Details',
+        overlay: { backgroundColor: "#000", opacity:0.9},
+      });
     });
   });
 
@@ -15,12 +22,5 @@ $(function(){
   $("#Map > *").click(function(){
     var link = ($(this).attr('id'));
     console.log(link);
-    $('.location-pop-up').dialog({
-      width: 600,
-      height: 'auto',
-      modal:true,
-      title: 'Location Details',
-      overlay: { backgroundColor: "#000", opacity:0.9},
-    });
   });
 });
