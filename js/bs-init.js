3248c60c8c920657452309ddc09aebbd1a81cd2f
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-03-17 09:54:08 +0000

    Merging some of the statements together

--- js/bs-init.js
+++ js/bs-init.js
@@ -1,29 +1,28 @@
-  $(document).ready(function() {
-    $('[data-toggle="tooltip"]').tooltip();
-  });
+$(document).ready(function() {
+  $('[data-toggle="tooltip"]').tooltip();
+});
 
-  $(document).ready(function() {
-    var allRegions = $("#Map > *");
+$(document).ready(function() {
+  var allRegions = $("#Map > *");
 
-    allRegions.on("click", function() {
-      allRegions.removeClass("on");
-      $(this).addClass("on");
-      $('.location-pop-up').dialog({
-        width: 600,
-        height: 'auto',
-        modal:true,
-        title: 'Location Details',
-        overlay: { backgroundColor: "#000", opacity:0.9},
-      });
+  allRegions.on("click", function() {
+    allRegions.removeClass("on");
+    $(this).addClass("on");
+    $('.location-pop-up').dialog({
+      width: 600,
+      height: 'auto',
+      modal:true,
+      title: 'Location Details',
+      overlay: { backgroundColor: "#000", opacity:0.9},
     });
   });
-
-$(function(){
-  $("#Map > *").click(function(){
-    var link = ($(this).attr('id'));
-    console.log(link);
-    if (link == "Yorkshire_and_the_Humber"){
-      document.getElementById('location-details').innerhtml = "<a href='https://www.aesseal.com/en/locations/europe/gb/SYK'<a>Click to see locations in your area</a>"
-    };
+  $(function(){
+    $("#Map > *").click(function(){
+      var link = ($(this).attr('id'));
+      console.log(link);
+      if (link == "Yorkshire_and_the_Humber"){
+        document.getElementById('location-details').innerhtml = "<a href='https://www.aesseal.com/en/locations/europe/gb/SYK'<a>Click to see locations in your area</a>";
+      }
+    });
   });
 });
