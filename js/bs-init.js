eab9e4f5224383bac51ee0c3ef2e8be967257a7a
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-03-22 10:37:10 +0000

    See if i can remove the highlight from the region after dialog closes

--- js/bs-init.js
+++ js/bs-init.js
@@ -22,6 +22,7 @@ $(document).ready(function() {
       },
     });
     $('.location-pop-up').on("dialogclose", function(){
+      allRegions.removeClass("on");
       if(link == "Yorkshire_and_the_Humber"){
         $('.millClose').removeClass('show');
       }
