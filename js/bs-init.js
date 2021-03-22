79570afa0af06e5a0128adb7ad641d2ec7e521a8
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-03-22 10:07:57 +0000

    Adding more to the remove class function

--- js/bs-init.js
+++ js/bs-init.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
       title: 'Location Details',
       overlay: { backgroundColor: "#000", opacity:0.9},
       close: function(){
-        link.removeClass("show");
+        $(link).removeClass("show");
       },
     });
   });
