998a6db17326509ac4d5f4e8be9b0479b938dfc4
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-03-22 11:30:26 +0000

    Updating the width of the dialog window to match more correctly

--- js/bs-init.js
+++ js/bs-init.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
     allRegions.removeClass("on");
     $(this).addClass("on");
     $('.location-pop-up').dialog({
-      width: 'auto',
+      width: $(window).width(),,
       height: 'auto',
       modal:true,
       title: 'Location Details',
