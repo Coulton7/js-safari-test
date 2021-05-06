4a206e83aa6f31280af1818bc511727da0be90af
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-05-06 16:04:47 +0100

    Testing new mthod of code and html for the counter

--- js/counter.js
+++ js/counter.js
@@ -1,7 +1,5 @@
 (function($){
 
-  import { CountUp } from '../js/countUp.min.js';
-
   $(document).ready(function() {
     const count = new CountUp('#counter', 353500000);
     if (!count.error) {
