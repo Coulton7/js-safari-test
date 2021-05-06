7a22106959c27d98152840abc087b995ed46fbec
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-05-06 16:55:11 +0100

    Adding more options and shifted some varabled around

--- js/counter.js
+++ js/counter.js
@@ -1,13 +1,15 @@
 (function($){
   document.addEventListener('DOMContentLoaded', function() {
     var options = {
+      startVal: 0,
+      duration: 5,
       useEasing: true,
       useGrouping: true,
       separator: ',',
       decimal: '.',
     };
 
-    var count = new CountUp('car-counter',  0, 353500000, 5, options);
+    var count = new CountUp('car-counter', 353500000, options);
     if (!count.error) {
       count.start();
     } else {
