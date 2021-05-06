c9709dc9e1d2db64fcf614ebf345b7b35dc5e01d
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-05-06 16:40:54 +0100

    Majot changes to the script to see if it will start working

--- js/counter.js
+++ js/counter.js
@@ -1,14 +1,13 @@
 (function($){
+  document.addEventListener('DOMContentLoaded', function() {
+    var options = {
+      useEasing: true,
+      useGrouping: true,
+      separator: ',',
+      decimal: '.',
+    };
 
-  var options = {
-    useEasing: true,
-    useGrouping: true,
-    separator: ',',
-    decimal: '.',
-};
-
-  $(window).load(function() {
-    const count = new CountUp('#counter', 353500000 , 0, 2.5, options);
+    var count = new CountUp('#counter', 353500000 , 0, 2.5, options);
     if (!count.error) {
       count.start();
     } else {
