7f1a2758e8bf18ec360c364bb38edbe4c1f0d9b0
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-05-07 10:19:31 +0100

    changing as order was incorrect

--- js/counter.js
+++ js/counter.js
@@ -1,13 +1,14 @@
 (function($){
   document.addEventListener('DOMContentLoaded', function() {
     var options = {
+      duration: 7,
       useEasing: true,
       useGrouping: true,
       separator: ',',
       decimal: '.',
     };
 
-    var count = new CountUp('car-counter', 0, 529500000, 7,  options);
+    var count = new CountUp('car-counter', 0, 529500000, 0, 7,  options);
     if (!count.error) {
       count.start();
     } else {
