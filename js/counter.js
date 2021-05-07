b98cbbc8af1c23298a8a99e0d52ec4ad7a5b9da6
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-05-07 09:36:04 +0100

    Chaning the duration to the options and duration

--- js/counter.js
+++ js/counter.js
@@ -1,14 +1,13 @@
 (function($){
   document.addEventListener('DOMContentLoaded', function() {
     var options = {
-      duration: 7,
       useEasing: true,
       useGrouping: true,
       separator: ',',
       decimal: '.',
     };
 
-    var count = new CountUp('car-counter', 0, 529500000, 0,  options);
+    var count = new CountUp('car-counter', 0, 529500000, 7,  options);
     if (!count.error) {
       count.start();
     } else {
