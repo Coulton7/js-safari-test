3724344d6dd5139aba098da5f0e09efdc2b56e0a
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-05-10 17:33:06 +0100

    Had the effect on twice causing the issue

--- js/slider-options.js
+++ js/slider-options.js
@@ -91,16 +91,6 @@
             }
         }
       },
-      after: function(slider){
-        var elements = document.getElementsByClassName('typewrite');
-        for (var i=0; i<elements.length; i++) {
-            var toRotate = elements[i].getAttribute('data-type');
-            var period = elements[i].getAttribute('data-period');
-            if (toRotate) {
-              new TxtType(elements[i], JSON.parse(toRotate), period);
-            }
-        }
-      },
       animation: "fade",
       slideshowSpeed: 12000,
       easing: "swing",
