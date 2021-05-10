bdc43305ba1af1a6e6921d53e62c110b1fc22ca2
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-05-10 15:18:11 +0100

    Adjusting the typewrite difference

--- js/bs-init.js
+++ js/bs-init.js
@@ -31,8 +31,7 @@ $(document).ready(function() {
   });
 });
 
-var element = document.getElementsByClassName('typewrite');
-var aText = element.attr('data-type')[0];
+var aText = $('typewrite').attr('data-type')[0];
 var iSpeed = 100; // time delay of print out
 var iIndex = 0; // start printing array at this posision
 var iArrLength = aText[0].length; // the length of the text array
