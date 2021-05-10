bb279566943387a37b54da9a3e44e19b72db2a4a
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-05-10 15:23:50 +0100

    Removing the 0 again

--- js/bs-init.js
+++ js/bs-init.js
@@ -34,7 +34,7 @@ $(document).ready(function() {
 var aText = $('typewrite').attr('data-type');
 var iSpeed = 100; // time delay of print out
 var iIndex = 0; // start printing array at this posision
-var iArrLength = aText[0].length; // the length of the text array
+var iArrLength = aText.length; // the length of the text array
 var iScrollAt = 20; // start scrolling up at this many lines
 
 var iTextPos = 0; // initialise text position
