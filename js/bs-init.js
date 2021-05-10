159de12c63dbfb5c864dbbeef1e9be90a44d2c4b
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-05-10 17:22:24 +0100

    Adding start effect to it need to remove jumpiness of it and moved vars

--- js/bs-init.js
+++ js/bs-init.js
@@ -30,37 +30,3 @@ $(document).ready(function() {
     });
   });
 });
-
-var TxtType = function(el, toRotate, period) {
-        this.toRotate = toRotate;
-        this.el = el;
-        this.loopNum = 0;
-        this.period = parseInt(period, 10) || 7000;
-        this.txt = '';
-        this.tick();
-        this.isDeleting = false;
-    };
-
-    TxtType.prototype.tick = function() {
-        var i = this.loopNum % this.toRotate.length;
-        var fullTxt = this.toRotate[i];
-
-        if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-        } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-        }
-
-        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-        var that = this;
-        var delta = 100 - Math.random() * 100;
-
-        setTimeout(function() {
-        that.tick();
-        }, delta);
-    };
-
-    window.onload = function() {
-        
-    };
--- js/slider-options.js
+++ js/slider-options.js
@@ -9,6 +9,36 @@
            (window.innerWidth < 1200) ? 4 : 4;
   }
 
+  var TxtType = function(el, toRotate, period) {
+          this.toRotate = toRotate;
+          this.el = el;
+          this.loopNum = 0;
+          this.period = parseInt(period, 10) || 7000;
+          this.txt = '';
+          this.tick();
+          this.isDeleting = false;
+      };
+
+      TxtType.prototype.tick = function() {
+          var i = this.loopNum % this.toRotate.length;
+          var fullTxt = this.toRotate[i];
+
+          if (this.isDeleting) {
+          this.txt = fullTxt.substring(0, this.txt.length - 1);
+          } else {
+          this.txt = fullTxt.substring(0, this.txt.length + 1);
+          }
+
+          this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+          var that = this;
+          var delta = 100 - Math.random() * 100;
+
+          setTimeout(function() {
+          that.tick();
+          }, delta);
+      };
+
   $(document).ready(function() {
 
     $('.flexslider').flexslider({
@@ -42,6 +72,16 @@
     $('.newsslider').flexslider({
       controlNav: false,
       directionNav: false,
+      start: function(slider){
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+      },
       before: function(slider){
         var elements = document.getElementsByClassName('typewrite');
         for (var i=0; i<elements.length; i++) {
