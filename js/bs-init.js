532e07fd28ced2f8e7bf6921e2b88baf00222ebe
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-05-10 16:20:44 +0100

    Adding missing semi colon

--- js/bs-init.js
+++ js/bs-init.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
     }
     allRegions.removeClass("on");
     $(this).addClass("on");
-    $('body').addClass("noscroll")
+    $('body').addClass("noscroll");
     $('.location-pop-up').dialog({
       width: "70%",
       height: 'auto',
