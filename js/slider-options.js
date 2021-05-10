d1c383462d46fcb2d37acc40b60c5a715c9521e3
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-05-10 10:35:13 +0100

    Adding new slider and animation effects for a slider type for testing

---
+++ js/slider-options.js
@@ -0,0 +1,79 @@
+(function($) {
+
+  var $window = $(window),
+  flexslider = { vars:{} };
+
+  function getGridSize() {
+    return (window.innerWidth < 768) ? 2 :
+           (window.innerWidth < 992) ? 3 :
+           (window.innerWidth < 1200) ? 4 : 4;
+  }
+
+  $(document).ready(function() {
+
+    $('.flexslider').flexslider({
+      controlNav: false,
+      directionNav: false
+
+    });
+
+    $('.controlNav').flexslider({
+      controlNav: true,
+      directionNav: false
+    });
+
+    $('.carousel-slider').flexslider({
+      controlNav: false,
+      directionNav: true,
+      animation: "slide",
+      easing: "swing",
+      animationLoop: true,
+      pauseOnHover: true,
+      itemWidth: 180,
+      itemMargin: 15,
+      minItems: getGridSize(),
+      maxItems: getGridSize(),
+      move: 1,
+      start: function(slider){
+        flexslider = slider;
+      }
+    });
+
+    $('.newsslider').flexslider({
+      controlNav: false,
+      directionNav: false,
+      animation: "fade",
+      slideshowSpeed: 12000,
+      easing: "swing",
+      animationLoop: true,
+
+    })
+
+    $('.product-slider').flexslider({
+      controlNav: false,
+      directionNav: false,
+      animation: "fade",
+      easing: "swing",
+      animationLoop: true,
+    });
+
+    $('.directionNav').flexslider({
+      controlNav: false,
+      directionNav: true
+    });
+
+    $('.base-slider').flexslider({
+      controlNav: false,
+      directionNav: false
+    });
+
+  });
+
+  $window.resize(function(){
+    var gridSize = getGridSize();
+
+    flexslider.vars.minItems = gridSize;
+    flexslider.vars.maxItems = gridSize;
+  });
+
+})(jQuery);
