8c026bc8d688315ff396398ba8f53f91b8416bf4
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-05-11 09:15:59 +0100

    Re-adding delete effect for testing on suttering

--- js/slider-options.js
+++ js/slider-options.js
@@ -33,6 +33,17 @@
           var that = this;
           var delta = 100 - Math.random() * 100;
 
+          if (this.isDeleting) { delta /= 2; }
+
+          if (!this.isDeleting && this.txt === fullTxt) {
+          delta = this.period;
+          this.isDeleting = true;
+          } else if (this.isDeleting && this.txt === '') {
+          this.isDeleting = false;
+          this.loopNum++;
+          delta = 500;
+          }
+
           setTimeout(function() {
           that.tick();
           }, delta);
