4f592e5eb35ef891d22d2a7bad1b4c0650432f28
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-05-12 17:04:50 +0100

    trying with slider.find

--- js/slider-options.js
+++ js/slider-options.js
@@ -60,7 +60,7 @@
       after: function(slider){
 
 
-        if ($(document.querySelectorAll('.flex-active-slide p.typewrite').innerHTML !== "")){
+        if (slider.find('.flex-active-slide p.typewrite').innerHTML !== ""){
           return false;
         } else {
           var curSlide = slider.find("li.flex-active-slide");
