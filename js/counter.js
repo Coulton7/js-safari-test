85eed14aefd04eede75978502d893642cc0ce459
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-05-13 10:40:45 +0100

    Trying new js to activate counter once visible

--- js/counter.js
+++ js/counter.js
@@ -8,10 +8,21 @@
     };
 
     var count = new CountUp('car-counter', 0, 529500000, 0, 7,  options);
-    if (!count.error) {
-      count.start();
-    } else {
-      console.error(count.error);
-    }
+
+    jQuery(window).on("resize scroll", function(){
+      var scrollPosition = jQuery(window).scrollTop();
+      var viewportHeight = window.innerHeight();
+      var offset = jQuery("#car-counter").offset();
+      var elementHeight = jQuery("#car-counter").height();
+      if(offset.top > scrollPosition - elementHeight && offset.top < scrollPosition + viewportHeight) {
+        if (!count.error) {
+          count.start();
+        } else {
+          console.error(count.error);
+        }
+      } else {
+        return false;
+      }
+    })
   });
 })(jQuery);
