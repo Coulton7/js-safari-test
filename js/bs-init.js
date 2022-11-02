50e1373727a2a55dfc88f93c75b4eb129303e088
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2022-11-02 10:41:34 +0000

    Printing the unicode symbol

--- js/bs-init.js
+++ js/bs-init.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
     $('body').addClass("noscroll");
     $('.location-pop-up').dialog({
       open: function(event, ui) {
-        $('.ui-dialog-titlebar-close').removeClass("ui-button-icon-primary ui-icon ui-icon-closethick").html('<span class="close-dialog"><i class="fas fa-bars"></i></span>');
+        $('.ui-dialog-titlebar-close').removeClass("ui-button-icon-primary ui-icon ui-icon-closethick").html('<span class="close-dialog">&#215;</span>');
         $('.ui-dialog-title').addClass('h2').addClass('text-center');
       },
       width: "70%",
