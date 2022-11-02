3a9857b46ad9aeb4d742b4dabd58555271ef1acf
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2022-11-02 09:33:48 +0000

    Added the new js varition for testing with the overlay

--- js/nav-btn.js
+++ js/nav-btn.js
@@ -13,6 +13,9 @@
             $('.search-block').removeClass('overlay-search');
             $('.navigation_col').removeClass('overlay-menu');
             $(y).addClass('fixed-top');
+            if($('#prodForm').data('modal').isShown) {
+              $('#prodForm').modal('hide')
+            }
         } else {
             x.style.height = "100%";
             $('.main').addClass('active');
@@ -23,7 +26,32 @@
             $(y).removeClass('fixed-top');
         }
     });
-    $("#mobile-overlay").click(
+
+    $("#footer-overlay-btn").click(
+      function footerNavbtn(){
+        document.getElementById('footer-overlay-btn').classList.toggle("change");
+        var x = document.getElementById('myNav');
+        var y = document.getElementById('main-nav');
+        if (x.style.height === "100%"){
+          x.style.height = "0%";
+          $('.main').removeClass('active');
+          $("body").removeClass('noscroll');
+          $('#overlay-btn').addClass('overlay-btn');
+          $('.search-block').removeClass('overlay-search');
+          $('.navigation_col').removeClass('overlay-menu');
+          $(y).addClass('fixed-top');
+        } else {
+          x.style.height = "100%";
+          $('.main').addClass('active');
+          $("body").addClass('noscroll');
+          $('#overlay-btn').removeClass('overlay-btn');
+          $('.search-block').addClass('overlay-search');
+          $('.navigation_col').addClass('overlay-menu');
+          $(y).removeClass('fixed-top');
+        }
+      });
+
+   /*$("#mobile-overlay").click(
     function mobileNavBtn(){
         document.getElementById('mobile-overlay').classList.toggle("change");
         var x = document.getElementById('myNav');
@@ -45,8 +73,9 @@
             $('.navigation_col').addClass('overlay-menu');
             $(y).removeClass('fixed-top');
         }
-    });
+    });*/
   });
+
   $(document).ready(function() {
     $('#prev').on('click', function() {
       $('.sustain-nav').animate({
