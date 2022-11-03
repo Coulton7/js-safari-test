6a7fd522f0a316b6216a83caff97b0a4306f1d23
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2022-11-03 15:29:45 +0000

    Testing new modal js variation

--- js/bs-init.js
+++ js/bs-init.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
   $('[data-toggle="tooltip"]').tooltip();
 });
+
+$(document).ready(function() {
+  var allRegions = $("#Map > *");
+
+  allRegions.on("click", function() {
+    var link = ($(this).attr('id'));
+    var county;
+    if (link == "Yorkshire_and_the_Humber") {
+      county = "Yorkshire and the Humber";
+      $('millClose').addClass('show');
+    }
+    allRegions.removeClass("on");
+    $('#location-details').modal('show');
+    $(this).addClass("on");
+    $('body').addClass("noscroll");
+  })
+});
